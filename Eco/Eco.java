a35619c91c29e78dfff8a7eb0b519f61b570e3c5
Artur Hashimoto <98194188+arturch2011@users.noreply.github.com>
2023-06-07 13:47:10 -0300

    Create Eco.java

---
+++ Eco/Eco.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Eco
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        long numero = scanner.nextLong();
+
+        System.out.println(numero);
+        scanner.close();
+    }
+}
